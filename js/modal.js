7c83cfca7820c4905c122ddf5475d9003b3a39c8
albina_biriukova <Alja.birjukova@gmail.com>
2025-11-01 06:07:11 +0100

    modal

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,24 @@
 (() => {
   const refs = {
     // Додати атрибут data-modal-open на кнопку відкриття
+    openModalBtn: document.querySelector("[data-menu-open]"),
+    // Додати атрибут data-modal-close на кнопку закриття
+    closeModalBtn: document.querySelector("[data-menu-close]"),
+    // Додати атрибут data-modal на бекдроп модалки
+    modal: document.querySelector("[data-menu]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
+    refs.modal.classList.toggle("is-open");
+  }
+})();
+(() => {
+  const refs = {
+    // Додати атрибут data-modal-open на кнопку відкриття
     openModalBtn: document.querySelector("[data-modal-open]"),
     // Додати атрибут data-modal-close на кнопку закриття
     closeModalBtn: document.querySelector("[data-modal-close]"),
@@ -14,26 +32,5 @@
   function toggleModal() {
     // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
     refs.modal.classList.toggle("is-open");
-  }
-})();
-
-(() => {
-  const refs = {
-    // Додати атрибут data-mobile-menu-open на кнопку відкриття
-    openModalBtn: document.querySelector("[data-mobile-menu-open]"),
-
-    // Додати атрибут data-mobile-menu-close на кнопку закриття
-    closeModalBtn: document.querySelector("[data-mobile-menu-close]"),
-
-    // Додати атрибут data-mobile-menu-modal на бекдроп модалки
-    modal: document.querySelector("[data-mobile-menu-modal]"),
-  };
-
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
-  function toggleModal() {
-    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
-    refs.modal.classList.toggle("is-visible");
   }
 })();
